386365d2fbf929d7aaa1d3c39b579a84c4ca7bc0
Matt Peebles <peebles.mm@gmail.com>
2020-08-29 15:25:40 -0700

    test workflow trigger

--- src/components/calendar/date/date.tsx
+++ src/components/calendar/date/date.tsx
@@ -5,7 +5,7 @@ export default class Date extends React.Component
     render()
     {
         return (
-            <div>Hello World</div>
+            <div>Hello, World!</div>
         ) 
     }
 }
