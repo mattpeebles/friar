d0029b4a5e84b8d08e3088cf795df964f49e874d
Matt Peebles <peebles.mm@gmail.com>
2020-08-29 17:37:34 -0700

    add react router and web config

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import Header from "./components/header/header";
-import Date from "./components/calendar/date/date";
-import './App.scss';
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App()
 {
-  const finnhub = require('finnhub');
-
-  const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-  api_key.apiKey = process.env.REACT_APP_FINNHUB_API_KEY;
-  const finnhubClient = new finnhub.DefaultApi();
-  
-  finnhubClient.companyBasicFinancials("AAPL", "margin", (error: any, data: any, response: any) =>
-  {
-    console.log(data)
-  });
-
   return (
-    <div className="App">
-      <Header></Header>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import logo from '../logo.svg';
+import Header from "../components/header/header";
+
+import "./Home.scss";
+export default class Home extends React.Component
+{
+    render()
+    {
+        const finnhub = require('finnhub');
+
+        const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+        api_key.apiKey = process.env.REACT_APP_FINNHUB_API_KEY;
+        const finnhubClient = new finnhub.DefaultApi();
+
+        finnhubClient.companyBasicFinancials("AAPL", "margin", (error: any, data: any, response: any) =>
+        {
+            console.log(data)
+        });
+
+        return (
+            <div className="App">
+                <Header></Header>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
+}
